476b10cff059ee1181746474da7c783bb84cdec2
lp42626 <lp42626@INHI2U-LDW3351.CORP.CYIENT.COM>
2019-10-18 17:07:05 +0530

    Some Changes

--- sitesurvey/src/main/java/com/cyient/model/Tower_Installation.java
+++ sitesurvey/src/main/java/com/cyient/model/Tower_Installation.java
@@ -10,9 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
